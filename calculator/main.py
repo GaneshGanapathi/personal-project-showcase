2c7e5d6eea34a1cac515cbf822107a38503a3b4f
GaneshGanapathi <131535058+GaneshGanapathi@users.noreply.github.com>
2025-12-06 17:57:26 +0530

    feat: py file for calculator application

---
+++ calculator/main.py
@@ -0,0 +1,21 @@
+
+class Calculator():
+
+    def addition(self,a:int,b:int)-> int:
+            return a+b
+    
+    def subtraction(self,a:int,b:int)-> int:
+            return a-b 
+    
+    def multiply(self,a:int,b:int)-> int:
+            return a*b
+        
+    def division(self,a:int,b:int)-> int:
+            return a/b
+
+cals= Calculator()
+# print(cals.addition(a,b,operator))
+
+# if __name__ == "__main__":
+#     main()
+
